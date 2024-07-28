584524e15140b0decceeff4328805b047e587379
Glitch (linen-snapdragon-apology) <none>
2024-07-28 22:24:25 +0000

    🐃🍐 Checkpoint ./app.js:69314363/319

--- app.js
+++ app.js
@@ -69,21 +69,19 @@ app.post('/interactions', async function (req, res) {
           
           const siteswap_embed = new EmbedBuilder()
           .setColor(0x0099FF)
-          .setTitle('Some title')
-          .setURL('https://discord.js.org/')
+          .setTitle('Siteswaps')
+          .setURL('https://jugglinglab.org/html/animinfo.html')
           .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
           .setDescription('Some description here')
           .setThumbnail('https://i.imgur.com/AfFp7pu.png')
           .addFields(
             { name: 'Regular field title', value: 'Some value here' },
             { name: '\u200B', value: '\u200B' },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
-            { name: 'Inline field title', value: 'Some value here', inline: true },
           )
           .addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
           .setImage('https://i.imgur.com/AfFp7pu.png')
           .setTimestamp()
-          .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+          .setFooter({ text: 'Basé sur le générateur jugglinglab.org', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
           
           // content: `Génération du Siteswaps ${pattern}`,
           
